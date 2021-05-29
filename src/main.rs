08981585f17926a7da52587f5f8907ea6a1fdc15
Greg Folker <gregfolker.dev@gmail.com>
2021-05-29 13:27:54 -0500

    feat: add support for a verbosity option at cli

--- src/main.rs
+++ src/main.rs
@@ -5,5 +5,9 @@ use::clap::{App, load_yaml};
 
 fn main() {
     let yaml = load_yaml!("cmd.yaml");
-    let _matches = App::from(yaml).get_matches();
+    let matches = App::from(yaml).get_matches();
+
+    let verbosity = matches.value_of("verbosity");
+
+    println!("Running app with verbosity={:?}", verbosity);
 }
