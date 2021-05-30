3c300f9bd961a89af4f9a8f15ddac2ffbc28f33e
Greg Folker <gregfolker.dev@gmail.com>
2021-05-30 11:37:37 -0500

    feat: only print parsed flags if `--verbosity` is set

--- src/main.rs
+++ src/main.rs
@@ -8,14 +8,12 @@ fn main() {
     let matches = App::from(yaml).get_matches();
 
     let verbosity = matches.value_of("verbosity").unwrap_or("off");
-
-    println!("Running app with verbosity={}", verbosity);
-
     let outfile = matches.value_of("outfile").unwrap_or("STDOUT");
-
-    println!("Running app with outfile={}", outfile);
-
     let runtime = matches.value_of("runtime").unwrap_or("0");
 
-    println!("Running app with runtime={}", runtime);
+    if verbosity != "off" {
+        println!("Running app with verbosity={}", verbosity);
+        println!("Running app with outfile={}", outfile);
+        println!("Running app with runtime={}", runtime);
+    }
 }
