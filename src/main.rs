6a07bdc4bae79f1728e34512d3707329da79766a
Greg Folker <gregfolker.dev@gmail.com>
2021-05-30 11:25:46 -0500

    fix: `cli.yaml` fails to open

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@
 use::clap::{App, load_yaml};
 
 fn main() {
-    let yaml = load_yaml!("cmd.yaml");
+    let yaml = load_yaml!("cli.yaml");
     let matches = App::from(yaml).get_matches();
 
     let verbosity = matches.value_of("verbosity").unwrap_or("off");
