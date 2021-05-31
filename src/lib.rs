42c890dad47a1ac9602616583842102a9ff6b1c9
Greg Folker <gregfolker.dev@gmail.com>
2021-05-31 14:39:32 -0500

    feat: allow user to specify number of execution threads, default to 1

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,33 @@
 use::std::thread;
 
 pub struct ThreadPool {
-    threads: Vec<thread::JoinHandle<()>>,
+    workers: Vec<Worker>,
 }
 
 impl ThreadPool {
     pub fn new(size: usize) -> ThreadPool {
         assert!(size > 0);
 
-        let mut threads = Vec::with_capacity(size);
+        let mut workers = Vec::with_capacity(size);
 
-        for _ in 0..threads.len() {
-            // TODO: Spawn threads
+        for id in 0..workers.len() {
+            workers.push(Worker::new(id));
+            println!("Created thread {}!", id);
         }
 
-        ThreadPool { threads }
+        ThreadPool { workers }
+    }
+}
+
+struct Worker {
+    id: usize,
+    thread: thread::JoinHandle<()>,
+}
+
+impl Worker {
+    pub fn new(id: usize) -> Worker {
+        let thread = thread::spawn(|| {});
+
+        Worker { id, thread }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -14,17 +14,18 @@ fn main() {
     let verbosity = matches.value_of("verbosity").unwrap_or("off");
     let outfile = matches.value_of("outfile").unwrap_or("STDOUT");
     let runtime: u64 = matches.value_of("runtime").unwrap_or("0").parse().expect("Expected a numerical value");
-    let thread_count: usize = matches.value_of("thread-count").unwrap_or("1").parse().expect("Expected a numerical value");
+    let thread_count: usize = matches.value_of("num-threads").unwrap_or("1").parse().expect("Expected a numerical value");
 
     if verbosity != "off" {
         println!("Running app with verbosity={}", verbosity);
         println!("Running app with outfile={}", outfile);
         println!("Running app with runtime={}", runtime);
+        println!("Running app with thread_count={}", thread_count);
      }
 
     let threads = ThreadPool::new(thread_count);
 
-    println!("Waiting for {} thread(s) to complete...", thread_count);
+    println!("Waiting for {} thread(s) to complete in {} second(s)...", thread_count, runtime);
     thread::sleep(Duration::from_millis(runtime * 1000));
     println!("Done!");
 }
