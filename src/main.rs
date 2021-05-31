f2587276cd4291eecc8f2785b4e9006ea2ae1ace
Greg Folker <gregfolker.dev@gmail.com>
2021-05-31 11:56:14 -0500

    feat: parse `--runtime` as a u64

--- src/main.rs
+++ src/main.rs
@@ -11,22 +11,21 @@ fn main() {
 
     let verbosity = matches.value_of("verbosity").unwrap_or("off");
     let outfile = matches.value_of("outfile").unwrap_or("STDOUT");
-    let runtime = matches.value_of("runtime").unwrap_or("0");
+    let runtime: u64 = matches.value_of("runtime").unwrap_or("0").parse().expect("Expected a numerical value");
 
     if verbosity != "off" {
         println!("Running app with verbosity={}", verbosity);
         println!("Running app with outfile={}", outfile);
         println!("Running app with runtime={}", runtime);
-    }
+     }
 
-    let worker = thread::Builder::new().spawn(move || {
+    let worker = thread::Builder::new().name("worker".to_string()).spawn(move || {
         println!("Hello from the worker thread!");
     }).unwrap();
 
     let _ = worker.join();
 
-    println!("Hello from the main execution thread!");
-    println!("Sleeping for 3 seconds...");
-    thread::sleep(Duration::from_millis(3000));
+    println!("Waiting for thread(s) to complete...");
+    thread::sleep(Duration::from_millis(runtime * 1000));
     println!("Done!");
 }
